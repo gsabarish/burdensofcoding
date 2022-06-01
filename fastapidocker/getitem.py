7b6c3063819a5b11b99bbdd85551a51a0901e831
Sabarish Gopalakrishnan <saby.gopal@gmail.com>
2022-06-01 01:14:50 -0700

    Add error for unknown itemid

--- fastapidocker/getitem.py
+++ fastapidocker/getitem.py
@@ -12,6 +12,9 @@ a_file.close()
 
 @router2.get('/get-item/{item_id}')
 def get_item(item_id):
+    if item_id not in json_object:
+        return{"Error": "Item ID does not exists."}
+
     return json_object[item_id]
 
 
